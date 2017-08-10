14cb2794999f6ee842747efb8b38ca95224a3b19
Ruslan <frodohrucik@gmail.com>
2017-08-10 11:16:46 +0300

    sdf

--- static/config.js
+++ static/config.js
@@ -1,7 +1,7 @@
-// var APP_CONFIG = {
-//     apiPrefix : 'http://localhost:8080/api/v1'
-// };
-
 var APP_CONFIG = {
-    apiPrefix : 'https://itsquiz.com/compliance/api/v1'
+    apiPrefix : 'http://localhost:8080/api/v1'
 };
+
+// var APP_CONFIG = {
+//     apiPrefix : 'https://itsquiz.com/compliance/api/v1'
+// };
