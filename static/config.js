8d96a7c3185314f1dcad53776dd171bf76c2c9b3
tropick <frodohrucik@gmail.com>
2017-08-21 12:16:38 +0300

    fix

--- static/config.js
+++ static/config.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 
 var APP_CONFIG = {
-    apiPrefix : 'http://localhost:8080/api/v1'
+    // apiPrefix : 'http://localhost:8080/api/v1'
+    apiPrefix : 'https://itsquiz.com/compliance/api/v1'
 };
