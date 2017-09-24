d636db56dd1963a20b42ddbf7cd7d240a9a639c9
tropick <frodohrucik@gmail.com>
2017-09-24 12:05:44 +0300

    asda

--- static/config.js
+++ static/config.js
@@ -3,8 +3,6 @@
 var APP_CONFIG = {
     // apiPrefix : 'http://localhost:8080/api/v1',
     // apiAiPrefix : 'http://localhost:8060/api/v1'
-    // apiPrefix : 'http://192.168.5.113:8080/api/v1'
-    // apiPrefix : 'https://itsquiz.com/compliance/api/v1'
     apiPrefix : 'https://itsquiz.com/compliance/api/v1',
     apiAiPrefix : 'https://app.itsquiz.ai/api/v1'
 };
