2984362294607928238ceecfbbfe24f40f2a077a
tropick <frodohrucik@gmail.com>
2017-09-25 15:40:07 +0300

    asdf

--- static/config.js
+++ static/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 var APP_CONFIG = {
-    // apiPrefix : 'http://localhost:8080/api/v1',
+    // apiPrefix : 'http://localhost:8080/api/v1', 
     // apiAiPrefix : 'http://localhost:8060/api/v1'
     apiPrefix : 'https://itsquiz.com/compliance/api/v1',
     apiAiPrefix : 'https://app.itsquiz.ai/api/v1'
