8c1913cef9b5775fa9388a50155f51204073f9c4
tropick <frodohrucik@gmail.com>
2017-09-26 10:53:02 +0300

    df

--- static/config.js
+++ static/config.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 
 var APP_CONFIG = {
-    // apiPrefix : 'http://localhost:8080/api/v1',
-    // apiAiPrefix : 'http://localhost:8060/api/v1'
-    apiPrefix : 'https://itsquiz.com/compliance/api/v1',
-    apiAiPrefix : 'https://app.itsquiz.ai/api/v1'
+    apiPrefix : 'http://localhost:8080/api/v1',
+    apiAiPrefix : 'http://localhost:8060/api/v1'
+    // apiPrefix : 'https://itsquiz.com/compliance/api/v1',
+    // apiAiPrefix : 'https://app.itsquiz.ai/api/v1'
 };
